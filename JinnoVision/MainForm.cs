1432f009a84581648ffdefae5f5a86a7f22603b7
user <user@LAPTOP-Q72316T3>
2025-12-17 15:08:15 +0800

    Add project files.

---
+++ JinnoVision/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using JinnoVision.Services;
+using System.Windows.Forms;
+
+namespace JinnoVision
+{
+    public partial class MainForm : Form
+    {
+        private readonly AuthResult _session;
+
+        public MainForm(AuthResult session)
+        {
+            InitializeComponent();
+            _session = session;
+            Text = $"Main - {_session.Username} (CompanyId={_session.CompanyId})";
+        }
+    }
+}
